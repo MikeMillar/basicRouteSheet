30895a9652ab313af1bef49864df9fc204ca71f9
Mike Millar <39285355+MikeMillar@users.noreply.github.com>
2019-12-15 11:05:31 -0700

    added new RO dialog
    added new popup dialog for new ro creation. currently does not create new ro instance

--- src/com/mikemillar/basicroutesheet/Controller.java
+++ src/com/mikemillar/basicroutesheet/Controller.java
@@ -1,6 +1,51 @@
 package com.mikemillar.basicroutesheet;
 
+import com.mikemillar.basicroutesheet.datamodels.RepairOrder;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.util.Optional;
+
 public class Controller {
+    
+    @FXML private BorderPane mainBorderPane;
 
-
+    public void initialize() {
+    
+    }
+    
+    public void showNewRODialog() {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainBorderPane.getScene().getWindow());
+        dialog.setTitle("Add New Repair Order");
+        dialog.setHeaderText("Use this dialog to add a new repair order");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("newRepairOrderDialog.fxml"));
+        try {
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+        } catch (IOException e) {
+            System.out.println("Could not load the dialog");
+            e.printStackTrace();
+            return;
+        }
+        
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+    
+        Optional<ButtonType> result = dialog.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            DialogController controller = fxmlLoader.getController();
+            RepairOrder newRO = controller.addRO();
+        }
+    }
+    
+    public void handleExit() {
+        Platform.exit();
+    }
+    
 }
---
+++ src/com/mikemillar/basicroutesheet/DialogController.java
@@ -0,0 +1,33 @@
+package com.mikemillar.basicroutesheet;
+
+import com.mikemillar.basicroutesheet.datamodels.RepairOrder;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class DialogController {
+    
+    @FXML private TextField roNumField;
+    @FXML private TextField tagNumField;
+    @FXML private TextField yearField;
+    @FXML private TextField makeField;
+    @FXML private TextField modelField;
+    @FXML private TextField mileField;
+    @FXML private TextField customerNameField;
+    @FXML private TextField phoneNumField;
+    @FXML private TextField jobsField;
+    @FXML private TextField adviserField;
+    @FXML private TextField techField;
+    @FXML private TextArea notesField;
+    @FXML private TextField dueTime;
+    @FXML private ChoiceBox waiterField;
+    
+    
+    public RepairOrder addRO() {
+        
+        
+        
+        return null;
+    }
+}

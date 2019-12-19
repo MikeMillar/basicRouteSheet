be65c96bad1e28526d65af57465d741b315d0b62
Mike Millar <39285355+MikeMillar@users.noreply.github.com>
2019-12-18 20:12:16 -0700

    add note class
    added note class

---
+++ src/com/mikemillar/basicroutesheet/datamodels/Note.java
@@ -0,0 +1,41 @@
+package com.mikemillar.basicroutesheet.datamodels;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Note {
+    
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm a MM/dd/yyyy");
+    
+    private RepairOrder ro;
+    private String note;
+    private LocalDateTime timeCreated;
+    private String timeCreatedString;
+    
+    public Note(RepairOrder ro, String note) {
+        this.ro = ro;
+        this.note = note;
+        this.timeCreated = LocalDateTime.now();
+        this.timeCreatedString = this.timeCreated.format(formatter);
+    }
+    
+    public void setNote(String note) {
+        this.note = note;
+    }
+    
+    public RepairOrder getRo() {
+        return ro;
+    }
+    
+    public String getNote() {
+        return note;
+    }
+    
+    public LocalDateTime getTimeCreated() {
+        return timeCreated;
+    }
+    
+    public String getTimeCreatedString() {
+        return timeCreatedString;
+    }
+}
